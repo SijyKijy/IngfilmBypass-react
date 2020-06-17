b1714aab84a4ba26844ff69b7f492198238161f8
SijyKijy <max.monovcov@gmail.com>
2020-06-18 00:17:01 +0300

    add site

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="header">
+      <p>Pro bypass</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/VideoForm.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class VideoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '', showVideo: false };
+
+    this.errorMsgRef = React.createRef();
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  checkUrl() {
+    const reg = /(\d*)-(.*)\.html/;
+    var res = reg.exec(this.state.value);
+    if (res != null) {
+      this.setState({ showVideo: true });
+      this.setState({ value: res[1] });
+    } else {
+      alert('Invalid link');
+    }
+  }
+
+  render() {
+    if (this.state.showVideo) {
+      return (
+        <iframe
+          title="BypassVideo"
+          src={`http://ingfilm.ru/vdb/` + this.state.value + `.html`}
+          width="720"
+          height="400"
+          frameborder="0"
+          allowfullscreen=""
+          style={{ border: false }}
+        />
+      );
+    } else {
+      return (
+        <div>
+          <input type="text" ref value={this.state.value} onChange={this.handleChange} />
+          <button
+            onClick={() => {
+              this.checkUrl();
+            }}>
+            Bypass
+          </button>
+        </div>
+      );
+    }
+  }
+}
+
+export default VideoForm;
