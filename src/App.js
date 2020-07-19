1401e477bb12ba76342850cdf38c9a7b3dfa77f0
SijyKijy <max.monovcov@gmail.com>
2020-07-19 18:38:07 +0300

    Component sorting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import Header from './Header';
-import Form from './VideoForm';
+import { Header, VideoForm } from './components';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Form />
+      <VideoForm />
     </div>
   );
 }
